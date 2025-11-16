ad97ac41460f535cea1e2d22bbf28863ef9d0e73
iqrok <blogo3x@gmail.com>
2025-11-16 07:36:38 +0700

    Create debug.hpp

---
+++ src/debug.hpp
@@ -0,0 +1,35 @@
+#ifndef _SFTP_NODE_DEBUG
+#define _SFTP_NODE_DEBUG
+
+#ifndef NDEBUG
+#	include <cassert> // for assert()
+
+/** Alias for SIGTRAP in debug mode. */
+#	if defined(__has_builtin) && __has_builtin(__builtin_debugtrap)
+#		define BREAKPOINT() __builtin_debugtrap()
+#	elif defined(_MSC_VER)
+#		define BREAKPOINT() __debugbreak()
+#	else
+#		include <signal.h>
+#		define BREAKPOINT() raise(SIGTRAP)
+#	endif
+
+#	define LOG_DBG(...) fprintf(stderr, __VA_ARGS__)
+
+#	define UNREACHABLE_MSG(...)                                               \
+		do {                                                                   \
+			LOG_DBG(__VA_ARGS__);                                              \
+			fprintf(                                                           \
+				stderr, "Should be Unreachable %s:%d\n", __FILE__, __LINE__);  \
+			BREAKPOINT();                                                      \
+		} while (0)
+
+#else
+
+#	define BREAKPOINT() ((void)0)
+#	define assert(...)  ((void)0)
+#	define LOG_DBG(...) ((void)0)
+
+#endif
+
+#endif
